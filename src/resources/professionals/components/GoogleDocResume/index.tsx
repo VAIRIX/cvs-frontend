fb97f78178107060ec6922d3a06db62e71a10aaf
Horacio Gonzalez <horacio@quipli.com>
2023-05-29 11:57:27 -0300

    feat: added loading bar to google doc preview
    fix: remove unused imports

--- src/resources/professionals/components/GoogleDocResume/index.tsx
+++ src/resources/professionals/components/GoogleDocResume/index.tsx
@@ -1,5 +1,5 @@
-import { Box, CircularProgress, Link } from '@mui/material';
-import { FC } from 'react';
+import { Box, CircularProgress, Link, LinearProgress } from '@mui/material';
+import { FC, useState } from 'react';
 import { SubsectionTitle } from 'components/ui';
 import { TEXTS } from 'constants/index';
 
@@ -9,6 +9,8 @@ type GoogleDocResumeProps = {
 };
 
 const GoogleDocResume: FC<GoogleDocResumeProps> = ({ loading, url }) => {
+  const [loadingPreview, setLoadingPreview] = useState(true);
+
   return (
     <Box sx={{ mt: 2, height: '100%' }}>
       <SubsectionTitle title={TEXTS.GOOGLE_DOC_LINK} />
@@ -19,25 +21,27 @@ const GoogleDocResume: FC<GoogleDocResumeProps> = ({ loading, url }) => {
         sx={{
           mt: 2,
           width: '100%',
-          justifyContent: 'center',
+          flexFlow: 'column',
           display: 'flex',
           height: '100%',
         }}
       >
-        {loading ? (
-          <CircularProgress />
-        ) : (
-          <iframe
-            src={url}
-            style={{
-              border: 'none',
-              height: '100%',
-              maxHeight: 800,
-              minHeight: 600,
-            }}
-            width="100%"
-          />
-        )}
+        {loading ? <CircularProgress /> : <></>}
+
+        {loadingPreview && url ? <LinearProgress /> : <></>}
+        <iframe
+          onLoad={() => {
+            setLoadingPreview(false);
+          }}
+          src={url}
+          style={{
+            border: 'none',
+            height: '100%',
+            maxHeight: 800,
+            minHeight: 600,
+          }}
+          width="100%"
+        />
       </Box>
     </Box>
   );
