ee30a895f443b3d50b613519efbc7c52a4f8b1fc
Leonardo Bellene <lbellene@vairix.com>
2023-07-16 12:13:34 -0300

    feat: remove next button in add experience modal

--- src/resources/professionals/components/ProjectsSection/Steps.tsx
+++ src/resources/professionals/components/ProjectsSection/Steps.tsx
@@ -392,11 +392,6 @@ const StepsDialog: FC<PropsWithChildren<StepsProps>> = ({
       <DialogActions>
         <Button onClick={handleClose}>Cancel</Button>
         {activeStep !== 0 && <Button onClick={handleBack}>Back</Button>}
-        {activeStep !== steps.length - 1 ? (
-          <Button onClick={handleNext}>Next</Button>
-        ) : (
-          <></>
-        )}
       </DialogActions>
     </Dialog>
   );
